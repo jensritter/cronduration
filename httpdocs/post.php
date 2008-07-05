e295fbbf9540bf18e61010294cafcbf0832cf7aa
jens.ritter@gmail.com <jens.ritter@gmail.com@de1dc89d-3551-0410-961e-0d9d67b8e0d6>
2008-07-05 17:07:33 +0000

    Bugfix + umgestellt auf SQLITE2

--- httpdocs/post.php
+++ httpdocs/post.php
@@ -8,13 +8,6 @@ if (!isset($_POST["command"])) {
 /* Settings : */
 $MAXFILESIZE = 5 * 1024;
 
-
-/* Database checks */
-if (!file_exists("data/post.db")) {
-	touch("data/post.db");
-	chmod("data/post.db",666);
-}
-
 /* Getting Data from Post */
 $file = $_FILES['upload']['tmp_name'];
 
@@ -25,14 +18,12 @@ $state	= $_POST["state"];
 
 /* one blob of sourcecode :*/
 
-if ($con = sqlite3_open('data/post.db')) {
+if ($con = sqlite_open('data/post.db')) {
 	/* create table, if !exists */
-	$q = @sqlite3_query($con,'SELECT host FROM events WHERE id = 1');
+	$q = @sqlite_query($con,'SELECT host FROM events WHERE id = 1');
 	if ($q === false) {
 		print "CREATE TABLE - ";
-		sqlite3_exec($con,'CREATE TABLE events ( id INTEGER PRIMARY KEY, host varchar(255), command varchar(255), state int, log TEXT, statedate date);');
-	} else {
-		sqlite3_query_close($q);
+		sqlite_exec($con,'CREATE TABLE events ( id INTEGER PRIMARY KEY, host varchar(255), command varchar(255), state int, log TEXT, statedate date);');
 	}
 
 	/* read logfile */
@@ -55,11 +46,11 @@ if ($con = sqlite3_open('data/post.db')) {
 	}
 
 
-   if (!sqlite3_exec($con,"insert into events(host,command,state,log,statedate) values('$host','$command','$state','$filecontent',DATETIME('now','localtime') );") ) {
-		print sqlite3_error($con) ;
+   if (!sqlite_exec($con,"insert into events(host,command,state,log,statedate) values('$host','$command','$state','$filecontent',DATETIME('now','localtime') );") ) {
+		print sqlite_error($con) ;
 		exit(0);
 	}
-	sqlite3_close($con);
+	sqlite_close($con);
 	print "INSERTED\n";
 } else {
 	print "ERROR\n";
